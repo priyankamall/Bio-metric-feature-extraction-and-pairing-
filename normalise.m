b1478bba6e3624dcc0793e5c13b5d5ef327fad4d
Priyanka Mall <87891538+PMall09@users.noreply.github.com>
2021-08-29 23:31:37 +0530

    Update normalise.m

--- normalise.m
+++ normalise.m
@@ -1,5 +1,4 @@
 function n = normalise(im, reqmean, reqvar)
-
     if ~(nargin == 1 | nargin == 3)
        error('No of arguments must be 1 or 3');
     end    
@@ -16,16 +15,12 @@ function n = normalise(im, reqmean, reqvar)
 	    n = im - min(im(:));
 	    n = n/max(n(:));
 	end
-	
     else  	
-	if ndims(im) == 3         % colour image?
+	if ndims(im) == 3   
 	    error('cannot normalise colour image to desired mean and variance');
 	end
-
 	if ~isa(im,'double'), im = double(im); end	
 	im = im - mean(im(:));    
-	im = im/std(im(:));      % Zero mean, unit std dev
-
+	im = im/std(im(:));   
 	n = reqmean + im*sqrt(reqvar);
-    end
-    
+    end  
